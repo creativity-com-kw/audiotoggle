5d8e974d778e996ef0a6774632be92b3ef1c3995
Aziz Bohra <aziz@creativity.com.kw>
2021-09-23 22:23:45 +0300

    fix for android webrtc echo

--- www/audiotoggle.js
+++ www/audiotoggle.js
@@ -10,3 +10,47 @@ exports.CALL_ON_SPEAKER = 'call_on_speaker';
 exports.setAudioMode = function (mode) {
 	cordova.exec(null, null, 'AudioTogglePlugin', 'setAudioMode', [mode]);
 };
+
+exports.displayIOSAudioRoutingComponent = function () {
+	cordova.exec(null, null, 'AudioTogglePlugin', 'displayIOSAudioRoutingComponent');
+};
+
+exports.hasAudioRoutingOptions = function (callback) {
+	cordova.exec(callback, null, 'AudioTogglePlugin', 'checkAudioRoutingOptions');
+};
+
+exports.setBluetoothScoOn = function (toggle) {
+	cordova.exec(null, null, 'AudioTogglePlugin', 'setBluetoothScoOn', [toggle]);
+};
+
+exports.setSpeakerphoneOn = function (toggle) {
+	cordova.exec(null, null, 'AudioTogglePlugin', 'setSpeakerphoneOn', [toggle]);
+};
+
+exports.getOutputDevices = function (callback) {
+	cordova.exec(callback, null, 'AudioTogglePlugin', 'getOutputDevices', []);
+};
+
+exports.on = function (eventName, callback) {
+	cordova.exec(callback, null, 'AudioTogglePlugin', 'registerListener', [eventName]);
+};
+
+exports.getAudioMode = function (callback) {
+	cordova.exec(callback, null, 'AudioTogglePlugin', 'getAudioMode', []);
+};
+
+exports.isBluetoothScoOn = function (callback) {
+	cordova.exec(callback, null, 'AudioTogglePlugin', 'isBluetoothScoOn', []);
+};
+
+exports.isSpeakerphoneOn = function (callback) {
+	cordova.exec(callback, null, 'AudioTogglePlugin', 'isSpeakerphoneOn', []);
+};
+
+exports.hasBuiltInEarpiece = function (successCb, errorCb) {
+	cordova.exec(successCb, errorCb, 'AudioTogglePlugin', 'hasBuiltInEarpiece');
+};
+
+exports.hasBuiltInSpeaker = function (successCb, errorCb) {
+	cordova.exec(successCb, errorCb, 'AudioTogglePlugin', 'hasBuiltInSpeaker');
+};
